d340925d761f4cd4d9917dd8c1a3edb453cc40c6
dvvostr <dv_vostr@mail.ru>
2023-12-26 16:55:50 +0300

    Initial Commit

---
+++ EM TestApp01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EM TestApp01
+//
+//  Created by Дмитрий Востриков on 26.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
